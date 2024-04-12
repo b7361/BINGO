94e2a6ce006b3c447bef99e18ce95d02a2ffed06
Boris <166778094+b7361@users.noreply.github.com>
2024-04-12 08:13:40 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,66 +1,48 @@
-var autoGenerateInterval;
-var numerosGenerados = [];
+// Crear la tabla con los números del 1 al 90
+var numerosContainer = document.getElementById("numeros");
+for (var i = 1; i <= 90; i++) {
+  var divNumero = document.createElement("div");
+  divNumero.textContent = i;
+  numerosContainer.appendChild(divNumero);
+}
 
 function generarNumeroAleatorio() {
-  var min = 1; // Valor mínimo
-  var max = 90; // Valor máximo
-
-  var numeroAleatorio;
-  do {
-    numeroAleatorio = Math.floor(Math.random() * (max - min + 1)) + min;
-  } while (numerosGenerados.includes(numeroAleatorio));
-
-  numerosGenerados.push(numeroAleatorio);
-  document.getElementById('resultado').innerText = " " + numeroAleatorio;
-
-  // Marcar el número aleatorio en rojo
-  var numeros = document.getElementsByClassName("numero");
-  for (var i = 0; i < numeros.length; i++) {
-    if (parseInt(numeros[i].innerText) === numeroAleatorio) {
-      numeros[i].classList.add("clicked");
-    }
-  }
+  var numero = Math.floor(Math.random() * 90) + 1;
+  document.getElementById("resultado").innerHTML = "" + numero;
+  marcarNumero(numero);
+  leerNumeroEnVoz(numero);
 }
 
 function toggleAutoGenerate() {
-  var autoGenerate = document.getElementById("autoGenerate").checked;
-
-  if (autoGenerate) {
-    autoGenerateInterval = setInterval(generarNumeroAleatorio, 3060); // Genera cada 4 segundos
+  var checkbox = document.getElementById("autoGenerate");
+  if (checkbox.checked) {
+    intervalId = setInterval(generarNumeroAleatorio, 3060);
   } else {
-    clearInterval(autoGenerateInterval);
-    document.getElementById("autoGenerate").checked = false; // Desmarcar la casilla de verificación
+    clearInterval(intervalId);
   }
 }
 
 function detenerGeneracionAutomatica() {
-  clearInterval(autoGenerateInterval);
-  document.getElementById("autoGenerate").checked = false; // Desmarcar la casilla de verificación
+  clearInterval(intervalId);
 }
 
-
-// Función para inicializar los números del 1 al 90
-function initNumeros() {
-  var numerosDiv = document.getElementById("numeros");
-  for (var i = 1; i <= 90; i++) {
-    var numero = document.createElement("div");
-    numero.innerText = i;
-    numero.classList.add("numero");
-    numero.onclick = function() {
-      var num = parseInt(this.innerText);
-      document.getElementById('resultado').innerText = "Número aleatorio: " + num;
-      var numeros = document.getElementsByClassName("numero");
-      for (var j = 0; j < numeros.length; j++) {
-        if (parseInt(numeros[j].innerText) === num) {
-          numeros[j].classList.add("clicked");
-        } else {
-          numeros[j].classList.remove("clicked");
-        }
-      }
-    };
-    numerosDiv.appendChild(numero);
+function marcarNumero(numero) {
+  var numeros = document.getElementById("numeros").children;
+  for (var i = 0; i < numeros.length; i++) {
+    if (parseInt(numeros[i].textContent) === numero) {
+      numeros[i].classList.add("marcado");
+      break;
+    }
   }
 }
 
-// Llamamos a la función de inicialización al cargar la página
-initNumeros();
+function leerNumeroEnVoz(numero) {
+  var mensaje = "" + numero;
+  var voz = new SpeechSynthesisUtterance(mensaje);
+  voz.lang = 'es-ES'; // Establece el idioma en español
+  window.speechSynthesis.speak(voz);
+}
+function detenerGeneracionAutomatica() {
+  clearInterval(intervalId);
+  document.getElementById("autoGenerate").checked = false;
+}
